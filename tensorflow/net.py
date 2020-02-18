fc01f936a266347894a40a4c094772f64da8ccab
aidarikako <120631909@qq.com>
2020-02-18 20:13:01 +0800

    add tensorflow

--- tensorflow/net.py
+++ tensorflow/net.py
@@ -7,8 +7,8 @@ from resnet import *
 class MyModel(tf.keras.Model):
     def __init__(self,classes=1):
         super(MyModel,self).__init__()
-        self.resnet = ResNet50(classes=classes,include_top=True,weights=None)
-        #self.resnet = ResNet50(classes=4,include_top=True,weights='imagenet')
+        #self.resnet = ResNet50(classes=classes,include_top=True,weights=None)
+        self.resnet = ResNet50(classes=4,include_top=True,weights='imagenet')
     
     def call(self,x):
         x=self.resnet(x)       
